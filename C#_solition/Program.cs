aed4e0e2fe460d27444ed87e77282b0b83cd04f0
Tim F <DEVILkin666@yandex.ru>
2023-10-06 01:26:54 +0600

    Create input array

---
+++ C#_solition/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine ("Введите длинну массива");
+int size = int.Parse (Console.ReadLine()!);
+string [] Array  = new string[size];
+Console.WriteLine ("Введите элементы массива");
+for (int i = 0; i < size; i++)
+{
+ Array [i] = Console.ReadLine()!;
+}
+ Console.WriteLine ("Исходный массив");
+Console.WriteLine ($"[{String.Join(',', Array)}]");
